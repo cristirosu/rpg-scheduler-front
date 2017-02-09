8d5f069baab19e58fd111d815ace48d4c55bdd1c
PETA-PC\Dragos Peta <petadragos@gmail.com>
2017-02-09 21:34:55 +0200

    calendar and task fixes

--- src/app/pages/dashboard/calendar/calendar.component.ts
+++ src/app/pages/dashboard/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation, Input, Output, ElementRef, EventEmitter} from '@angular/core';
 
 import {CalendarService} from './calendar.service';
 
@@ -9,50 +9,14 @@ import {CalendarService} from './calendar.service';
   template: require('./calendar.html')
 })
 export class Calendar {
+ 
+  @Input() calendarConfiguration:Object;
+  @Output() onCalendarReady = new EventEmitter<any>();
 
-  public calendarConfiguration:any;
-  private _calendar:Object;
+  @ViewChild('calendar') private _selector:ElementRef;
 
-  constructor(private _calendarService:CalendarService) {
-    this.calendarConfiguration = this._calendarService.getData();
-    this.calendarConfiguration.select = (start, end) => this._onSelect(start, end);
-    this.calendarConfiguration.eventDragged = (event, delta, revertFunc, jsEvent, ui, view) => {
-
-      alert(event.title + " was dropped on " + event.start.format());
-
-      if (!confirm("Are you sure about this change?")) {
-        revertFunc();
-      }
-
-    }
-  }
-
-  getNewData(){
-    console.log(":)):((");
-    jQuery(this._calendar).fullCalendar('removeEvents');
-
-    console.log("here a");
-  }
-
-  public onCalendarReady(calendar):void {
-    console.log("here b");
-    this._calendar = calendar;
-  }
-
-  private _onSelect(start, end):void {
-
-    if (this._calendar != null) {
-      let title = prompt('Event Title:');
-      let eventData;
-      if (title) {
-        eventData = {
-          title: title,
-          start: start,
-          end: end
-        };
-        jQuery(this._calendar).fullCalendar('renderEvent', eventData, true);
-      }
-      jQuery(this._calendar).fullCalendar('unselect');
-    }
+  ngAfterViewInit() {
+    let calendar = jQuery(this._selector.nativeElement).fullCalendar(this.calendarConfiguration);
+    this.onCalendarReady.emit(calendar);
   }
 }
